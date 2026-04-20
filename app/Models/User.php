8887bcd2e531d6f2c7ab2bd6c9d174200d5e4031
Shivam Yadav <shivamtikkha@gmail.com>
2026-04-20 10:27:27 +0530

    model and migration setup

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,18 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function cart()
+{
+    return $this->hasOne(Cart::class);
+}
+
+public function orders()
+{
+    return $this->hasMany(Order::class);
+}
+
+public function addresses()
+{
+    return $this->hasMany(Address::class);
+}
 }
